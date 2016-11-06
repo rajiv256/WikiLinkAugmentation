3765280dda998f5efcb2b61452467942e8562611
sahitilucky <sahitilucky21@gmail.com>
2016-11-06 23:08:59 +0530

    article sim index changes

--- ArticlesSimIndex.py
+++ ArticlesSimIndex.py
@@ -2,18 +2,21 @@
 Article : (content ,summary , hyperlinks, see also, Categories)
 
 '''
-
+from ArticleClass import *
 from tfidf import *
+from parser import *
 
+def articleSimilarity(article1,article2):
+    #Tfidf1 = TfIdf(article1.summary)
+    #Tfidf2 = TfIdf(article2.summary)
 
-def similarity(article1,article2):
-    Tfidf1 = TfIdf(article1.summary)
-    Tfidf2 = TfIdf(article2.summary)
-
-    Sim1 = CosSim(Tfidf1,Tfidf2)
+    Sim1 = CosSim(article1.summryTfidf,article2.summryTfidf)
+    if(article1.summaryhyperlinks == []):
+        article1 = fill_links(article1.title)
+    if(article2.summaryhyperlinks == []):
+        article2 = fill_links(article2.title)
     links1 = map(lambda p : p[0] ,article1.summaryhyperlinks())
     links2 = map(lambda p: p[0], article2.summaryhyperlinks())
-
     linkidf1 = linkTfIdf(links1)
     linkidf2 = linkTfIdf(links2)
     Sim2 = CosSim(linkidf1,linkidf2)
@@ -22,8 +25,8 @@ def similarity(article1,article2):
     return (alpha*Sim1 + beta*Sim2)
 
 def artcatsimilarity(category,article2):
-    Tfidf1 = TfIdf(category.content)
-    Tfidf2 = TfIdf(article2.content)
+    #Tfidf1 = TfIdf(category.content)
+    #Tfidf2 = TfIdf(article2.content)
 
-    Sim1 = CosSim(Tfidf1,Tfidf2)
+    Sim1 = CosSim(category.contentTfidf,article2.contentTfidf)
     return Sim1
