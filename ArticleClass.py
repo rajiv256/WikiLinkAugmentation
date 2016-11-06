ce72bff5ef15473984af6a52b0c99f098e9aeeac
nvsskchaitanya <cs13b021@smail.iitm.ac.in>
2016-11-06 23:13:11 +0530

    variable file is created and content.py is using class.

--- ArticleClass.py
+++ ArticleClass.py
@@ -1,13 +1,24 @@
+from variable import *;
 
-"Class Article"
-
-class Article:
+class Article(object):
+    '''Article Class'''
     title = ""
     content  = ""
     summary = ""
+    contentTfIdf = {}
+    summryTfIdf = {}
     summaryhyperlinks =[] #(word :link)
     hyperlinks = []
     SeeAlso = []
     Categories = []
     PrunedCategories = []
 
+    def __init__(self, title):
+        self.title = title;
+        if title not in allTfIdf.keys():
+            self.contentTfIdf = TfIdf(givePrunedContent(title));
+            self.summryTfIdf = TfIdf(giveSummary(title));
+            allTfIdf[title] = (self.contentTfIdf,self.summryTfIdf);
+        else:
+            self.contentTfIdf = allTfIdf[title][0]
+            self.summryTfIdf = allTfIdf[title][1]
